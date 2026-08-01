c27e7647caac1cf086f36ba264ccbb44a700266a
Sumit Sudam Bhor <sumitbhor227@gmail.com>
2026-08-01 13:26:43 +0530

    basic dsa pattern prining

---
+++ c++/greenhouse/Patterns/Square.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+// int main(){
+//     int n=5;
+//     for(int i=0; i<n; i++){
+//         for (int j = 0; j<n; j++)
+//         {
+//             cout<<"*" ;
+//         }
+//        cout<<endl ; 
+//     }
+// }
+
+// int main(){
+//     int n=5;
+//     for(int i=0; i<n; i++){
+//         for (int j =1 ; j<n+1; j++)
+//         {
+//             cout<<j ;
+//         }
+//        cout<<endl ; 
+//     }
+// }
+
+// int main(){
+//     int n=5;
+//     for(int i=0; i<n; i++){
+//         char ch = 'A';
+//         for (int j =0 ; j<n; j++)
+//         {
+//             cout<<ch ;
+//             ch++;
+//         }
+//        cout<<endl ; 
+//     }
+// }
+
+int main(){
+    int n=3;
+    int num =1;
+    for(int i=0; i<n; i++){
+        for (int j =1 ; j<n+1; j++)
+        {
+            cout<<num ;
+            num++;
+        }
+       cout<<endl ; 
+    }
+}
